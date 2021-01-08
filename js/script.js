bfadaf91bb9ab6252be3df4ab0ebe7aa07c14751
Federico <crudofederico@gmail.com>
2021-01-08 16:22:09 +0100

    select image on click-work in progress

---
+++ js/script.js
@@ -0,0 +1,64 @@
+$(document).ready(function(){
+  //click on angle-right
+  $('.next').click(pulsanteNext); //funzione di callback che parte dopo il click
+  $('.prev').click(pulsantePrev);
+  //click on circle
+  $('.nav i').click(function(){
+    $('.nav i.active').removeClass('active');
+    $(this).addClass('active');
+    $('.images img').eq($(this).index());
+
+  });
+  
+
+});
+
+function pulsanteNext(){
+
+  
+  var imgAttiva = $('.images img.active');
+  console.log(imgAttiva);
+
+
+  var pallinoAttivo = $('.nav i.active');
+
+  imgAttiva.removeClass('active');
+  imgAttiva.next().addClass('active');
+
+  pallinoAttivo.removeClass('active');
+  pallinoAttivo.next().addClass('active');
+
+  if(imgAttiva.hasClass('last')){
+    $('.images img.first').addClass('active');
+    $('.nav i.first').addClass('active');
+  }
+
+}
+function pulsantePrev(){
+  var imgAttiva = $('.images img.active');
+  console.log(imgAttiva);
+ 
+
+  var pallinoAttivo = $('.nav i.active');
+
+  imgAttiva.removeClass('active');
+  imgAttiva.prev().addClass('active');
+
+  pallinoAttivo.removeClass('active');
+  pallinoAttivo.prev().addClass('active');
+
+  if(imgAttiva.hasClass('first')){
+    $('.images img.last').addClass('active');
+    $('.nav i.last').addClass('active');
+  }
+
+}
+$(document).keydown(function(e){
+  if (e.which == 39) { //right
+   pulsanteNext();
+  }
+  if (e.which == 37) { //left
+    pulsantePrev();
+   }
+
+});
